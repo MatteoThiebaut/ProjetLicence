9ea00c46dd93344cad26b8ec2925ef60ca54a7c5
MatteoThiebaut <mathi>
2021-10-27 11:46:24 +0200

    NomBDD

--- Model/bdd.php
+++ Model/bdd.php
@@ -7,7 +7,7 @@ class Bdd
     public function __construct()
     {
         try {
-            $this->dbh = new PDO("mysql:dbname=ppe;host=127.0.0.1;charset=utf8", "root", "");
+            $this->dbh = new PDO("mysql:dbname=bd_caf_1;host=127.0.0.1;charset=utf8", "root", "");
         } catch (PDOException $e) {
             echo 'Connexion échouée : ' . $e->getMessage();
             // Fonction connexion à la base de données si la connexion ne fonctionne pas 
